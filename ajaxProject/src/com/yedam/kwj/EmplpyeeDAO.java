33fcf14a03542c00f0ef596f80d42eed39acbca9
KwonWJ <dnwns93@naver.com>
2020-11-05 17:32:46 +0900

    20201105

--- ajaxProject/src/com/yedam/kwj/EmplpyeeDAO.java
+++
@@ -1,5 +0,0 @@
-package com.yedam.kwj;
-
-public class EmplpyeeDAO {
-
-}
--- ajaxProject/src/com/yedam/kwj/Sample.java
+++
@@ -1,9 +0,0 @@
-package com.yedam.kwj;
-
-public class Sample {
-	public static void main(String[] args) {
-		System.out.println("hello");
-
-	}
-
-}
